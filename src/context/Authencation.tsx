a835ddaba5300d541ca345f8a53e65d197babdd0
ItsYahBoiLaurence <laurence@mayan.com.ph>
2025-03-03 13:04:58 +0800

    Authencation Provider Modification

--- src/context/Authencation.tsx
+++ src/context/Authencation.tsx
@@ -4,34 +4,33 @@ import { useLocation, useNavigate } from "react-router-dom";
 import { auth } from "../api/firebaseConfig";
 
 type Creds = {
-    email: string,
-    password: string
-}
+    email: string;
+    password: string;
+};
 
 interface AuthContextType {
-    user: User | null
+    user: User | null;
     login: (credentials: Creds) => Promise<void>;
-    logout: () => Promise<void>
+    logout: () => Promise<void>;
 }
 
 export const AuthenticationContext = createContext<AuthContextType>({
     user: null,
     login: () => {
-        throw new Error('login method not implemented');
+        throw new Error("login method not implemented");
     },
     logout: () => {
-        throw new Error('logout method not implemented');
+        throw new Error("logout method not implemented");
     }
-})
+});
 
-const EXCLUDED_PATHS = [
-    '/sign-in'
-]
+const EXCLUDED_PATHS = ["/sign-in"];
 
 export const Authentication = ({ children }: PropsWithChildren<{}>) => {
-    const [user, setUser] = useState<User | null>(null)
-    const location = useLocation()
-    const navigate = useNavigate()
+    const [user, setUser] = useState<User | null>(null);
+    const location = useLocation();
+    const navigate = useNavigate();
+
 
     const login = async (credentials: Creds) => {
         try {
@@ -40,47 +39,49 @@ export const Authentication = ({ children }: PropsWithChildren<{}>) => {
                 credentials.email,
                 credentials.password
             );
-            setUser(userCredential.user)
-            const token = await userCredential.user.getIdToken()
-            localStorage.setItem('ADMIN_TOKEN', token)
+            setUser(userCredential.user);
+            const token = await userCredential.user.getIdToken();
+            localStorage.setItem("ADMIN_TOKEN", token);
         } catch (error) {
-            // Throw the entire error object for onError to inspect
             throw error;
         }
-    }
+    };
+
 
     const logout = async (): Promise<void> => {
         try {
             await signOut(auth);
             setUser(null);
             localStorage.removeItem("ADMIN_TOKEN");
-            navigate('/sign-in')
+            localStorage.removeItem("token");
+            navigate("/sign-in");
         } catch (error) {
             console.error("Logout failed", error);
             throw error;
         }
     };
 
+
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-            setUser(currentUser)
-        })
-        return () => unsubscribe()
-    }, [])
+            setUser(currentUser);
+        });
+
+        return () => unsubscribe();
+    }, []);
+
 
     useEffect(() => {
-        if (!user && !EXCLUDED_PATHS.includes(location.pathname)) {
-            navigate("/sign-in");
-        } else if (user && EXCLUDED_PATHS.includes(location.pathname)) {
+        if (user && EXCLUDED_PATHS.includes(location.pathname)) {
             navigate("/");
+        } else if (!user && !EXCLUDED_PATHS.includes(location.pathname)) {
+            navigate("/sign-in");
         }
-
-    }, [user, location.pathname, navigate])
-
+    }, [user, location.pathname, navigate]);
 
     return (
         <AuthenticationContext.Provider value={{ user, login, logout }}>
             {children}
         </AuthenticationContext.Provider>
-    )
-}
+    );
+};
