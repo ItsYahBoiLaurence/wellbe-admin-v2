ce865d3c2b31dd130f6fa387c55d904b058a3d2f
ItsYahBoiLaurence <laurence@mayan.com.ph>
2025-03-14 15:50:44 +0800

    Fixed Layout for Scatterplot

--- src/pages/scatterplot/index.tsx
+++ src/pages/scatterplot/index.tsx
@@ -1,5 +1,5 @@
 import { useForm } from 'react-hook-form';
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect, useCallback } from 'react';
 import { useQuery } from 'react-query';
 import { ScatterChart } from '@mantine/charts';
 import { Dropzone } from '@mantine/dropzone';
@@ -20,22 +20,19 @@ interface FormValues {
   file: File[];
 }
 
-const user_company = localStorage.getItem("USER_COMPANY")
+const user_company = localStorage.getItem("USER_COMPANY");
+
 const fetchDataForScatterPlot = async () => {
-  const company = user_company;
-  const params = { company };
+  const params = { company: user_company };
   try {
-    const response = await api.get(
-      '/api/company-admin/performanceScatterPlot/',
-      { params }
-    );
+    const response = await api.get('/api/company-admin/performanceScatterPlot/', { params });
     return response.data;
   } catch (error) {
     throw error;
   }
 };
 
-const formatData = (color, name, data) => [
+const formatData = (color: string, name: string, data: any[]) => [
   {
     color,
     name,
@@ -44,44 +41,25 @@ const formatData = (color, name, data) => [
 ];
 
 const Scatterplot = () => {
-  // All hooks are declared at the top
   const [notif, setNotif] = useState(false);
   const [errorNotif, setErrorNotif] = useState(false);
-  const { data, isLoading, error } = useQuery({
-    queryKey: ['scatterPlot'],
-    queryFn: fetchDataForScatterPlot,
-  });
+  const { data, isLoading, error } = useQuery('scatterPlot', fetchDataForScatterPlot);
   const theme = useMantineTheme();
   const openRef = useRef(null);
   const [opened, setOpened] = useState(false);
-  const {
-    handleSubmit,
-    setValue,
-    watch,
-    reset,
-    formState: { errors },
-  } = useForm<FormValues>({ defaultValues: { file: [] } });
+  const { handleSubmit, setValue, watch, reset, formState: { errors } } = useForm<FormValues>({ defaultValues: { file: [] } });
   const fileValue = watch('file');
 
-  // Handlers remain the same...
-  const handleDrop = (files: File[]) => {
-    if (files.length > 0 && files[0].type === 'text/csv') {
-      setValue('file', files);
-    } else {
-      setValue('file', []);
-    }
-  };
+  const handleDrop = useCallback((files: File[]) => {
+    setValue('file', files.length > 0 && files[0].type === 'text/csv' ? files : []);
+  }, [setValue]);
 
-  const onSubmit = async (values: FormValues) => {
+  const onSubmit = useCallback(async (values: FormValues) => {
     if (values.file && values.file.length > 0) {
       const formData = new FormData();
       formData.append('file', values.file[0]);
       try {
-        await api.post(
-          `/api/company-admin/batchUploadPerformance?company=${user_company}`,
-          formData,
-          { headers: { 'Content-Type': 'multipart/form-data' } }
-        );
+        await api.post(`/api/company-admin/batchUploadPerformance?company=${user_company}`, formData, { headers: { 'Content-Type': 'multipart/form-data' } });
         setNotif(true);
         reset({ file: [] });
       } catch (error) {
@@ -89,7 +67,7 @@ const Scatterplot = () => {
         setErrorNotif(true);
       }
     }
-  };
+  }, [reset]);
 
   useEffect(() => {
     if (notif) {
@@ -105,103 +83,69 @@ const Scatterplot = () => {
     }
   }, [errorNotif]);
 
-  // Instead of early returns, conditionally render content within JSX:
   return (
     <>
-      {/* Conditionally display loading or error states */}
+      <Paper radius="sm" shadow="xs" p="md" style={{ display: 'flex', justifyContent: 'flex-end' }} mb='md'>
+        <Button variant="filled" color="gray" onClick={() => setOpened(true)}>
+          Upload Performance Data
+        </Button>
+      </Paper>
+      <Drawer opened={opened} onClose={() => setOpened(false)} position="right">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Stack gap="md" p="md">
+            <Dropzone openRef={openRef} onDrop={handleDrop} accept={['text/csv']} maxSize={30 * 1024 ** 2}>
+              <div style={{ pointerEvents: 'none' }}>
+                <Group>
+                  <Dropzone.Accept>
+                    <IconDownload size={50} color={theme.colors.blue[6]} stroke={1.5} />
+                  </Dropzone.Accept>
+                  <Dropzone.Reject>
+                    <IconX size={50} color={theme.colors.red[6]} stroke={1.5} />
+                  </Dropzone.Reject>
+                  <Dropzone.Idle>
+                    <IconCloudUpload size={50} stroke={1.5} />
+                  </Dropzone.Idle>
+                </Group>
+                <Text ta="center" fw={700} size="lg" mt="xl">
+                  <Dropzone.Accept>Drop CSV file here</Dropzone.Accept>
+                  <Dropzone.Reject>Only CSV files under 30MB are accepted</Dropzone.Reject>
+                  <Dropzone.Idle>Upload CSV File</Dropzone.Idle>
+                </Text>
+                {fileValue && fileValue.length > 0 ? (
+                  <Text ta="center" size="sm" mt="md" fw={600}>
+                    Uploaded file: {fileValue[0].name}
+                  </Text>
+                ) : (
+                  <Text ta="center" size="sm" mt="xs" color="dimmed">
+                    Drag & drop a CSV file here. Only <i>.csv</i> files less than 30MB are accepted.
+                  </Text>
+                )}
+              </div>
+            </Dropzone>
+            {errors.file && (
+              <Text color="red" size="sm">
+                {errors.file.message || 'Please upload a CSV file'}
+              </Text>
+            )}
+            {notif && <Text c="green">Upload Success!</Text>}
+            {errorNotif && <Text c="red">Account already registered!</Text>}
+            <Button type="submit" variant="filled" color="gray">
+              Upload Performance Data
+            </Button>
+          </Stack>
+        </form>
+      </Drawer>
+
       {isLoading ? (
         <Text>Loading...</Text>
       ) : error ? (
         <Paper w='100%' p="md"><Center><Text>No data available!</Text></Center></Paper>
       ) : (
         <>
-          <Paper
-            radius="sm"
-            shadow="xs"
-            p="md"
-            style={{ display: 'flex', justifyContent: 'flex-end' }}
-            mb='md'
-          >
-            <Button
-              variant="filled"
-              color="gray"
-              onClick={() => setOpened(true)}
-            >
-              Upload Performance Data
-            </Button>
-          </Paper>
-          <Drawer
-            opened={opened}
-            onClose={() => setOpened(false)}
-            position="right"
-          >
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <Stack gap="md" p="md">
-                <Dropzone
-                  openRef={openRef}
-                  onDrop={handleDrop}
-                  accept={['text/csv']}
-                  maxSize={30 * 1024 ** 2} // 30MB
-                >
-                  <div style={{ pointerEvents: 'none' }}>
-                    <Group>
-                      <Dropzone.Accept>
-                        <IconDownload
-                          size={50}
-                          color={theme.colors.blue[6]}
-                          stroke={1.5}
-                        />
-                      </Dropzone.Accept>
-                      <Dropzone.Reject>
-                        <IconX
-                          size={50}
-                          color={theme.colors.red[6]}
-                          stroke={1.5}
-                        />
-                      </Dropzone.Reject>
-                      <Dropzone.Idle>
-                        <IconCloudUpload size={50} stroke={1.5} />
-                      </Dropzone.Idle>
-                    </Group>
-
-                    <Text ta="center" fw={700} size="lg" mt="xl">
-                      <Dropzone.Accept>Drop CSV file here</Dropzone.Accept>
-                      <Dropzone.Reject>
-                        Only CSV files under 30MB are accepted
-                      </Dropzone.Reject>
-                      <Dropzone.Idle>Upload CSV File</Dropzone.Idle>
-                    </Text>
-                    {fileValue && fileValue.length > 0 ? (
-                      <Text ta="center" size="sm" mt="md" fw={600}>
-                        Uploaded file: {fileValue[0].name}
-                      </Text>
-                    ) : (
-                      <Text ta="center" size="sm" mt="xs" color="dimmed">
-                        Drag & drop a CSV file here. Only <i>.csv</i> files less
-                        than 30MB are accepted.
-                      </Text>
-                    )}
-                  </div>
-                </Dropzone>
-                {errors.file && (
-                  <Text color="red" size="sm">
-                    {errors.file.message || 'Please upload a CSV file'}
-                  </Text>
-                )}
-                {notif && <Text c="green">Upload Success!</Text>}
-                {errorNotif && <Text c="red">Account already registered!</Text>}
-                <Button type="submit" variant="filled" color="gray">
-                  Upload Performance Data
-                </Button>
-              </Stack>
-            </form>
-          </Drawer>
           <Stack gap="md">
             <Paper radius="sm" shadow="xs" p="md">
               <Stack gap="md">
-                <Text size="xl">
-                  Performance vs. Well-being of the Employee
-                </Text>
+                <Text size="xl">Performance vs. Well-being of the Employee</Text>
                 <ScatterChart
                   h={350}
                   data={formatData('blue', 'Employee', data)}
