7c81e758664cdca6841c1f1c0763c58e5a7460ac
ItsYahBoiLaurence <laurence@mayan.com.ph>
2025-07-11 13:36:01 +0800

    Updated Insight Card

--- src/components/Cards/InsightCard.tsx
+++ src/components/Cards/InsightCard.tsx
@@ -1,4 +1,4 @@
-import { Badge, Box, Button, ColorSwatch, Group, LoadingOverlay, Pagination, Paper, Pill, Progress, SimpleGrid, Stack, Text, Title } from "@mantine/core"
+import { Badge, Box, Button, ColorSwatch, Flex, Group, LoadingOverlay, Pagination, Paper, Pill, Progress, SimpleGrid, Stack, Text, Title } from "@mantine/core"
 import { IconChevronDown, IconInfoSquareRounded } from '@tabler/icons-react';
 import { useQuery } from "react-query";
 import { ErrorBoundary } from 'react-error-boundary';
@@ -28,6 +28,14 @@ interface Question {
     id: number;
     question: string;
     domain: string;
+    interpretation: Interpretation[]
+}
+
+interface Interpretation {
+    content: string;
+    id: string;
+    question_id: number;
+    score_band: string;
 }
 
 interface OneSingleType {
@@ -59,28 +67,30 @@ function chunk<T>(array: T[], size: number): T[][] {
 
 const PaginatedData = ({ dataArray }: { dataArray: QuestionAnswerTally[] }) => {
 
-    const tip = "Many employees feel proud to be part of their organization, indicating strong workplace engagement. To build on this, leadership can highlight employee achievements regularly and foster open communication to sustain a positive work environment."
     const [activeCard, setActiveCard] = useState<number | null>(null)
     const [activePage, setPage] = useState(1);
     const chunkedData = chunk(dataArray, 3);
 
+    console.log("=----THIS IS START OF THE DATA----=")
+    console.log(chunkedData)
+    console.log("=----THIS IS END OFTHE DATA----=")
+
     const items = <SimpleGrid cols={3} w={'100%'}>
         {chunkedData[activePage - 1].map(({ question, answer }, index) => (
             <Paper py={'xl'} px={'lg'} key={index} shadow="xs">
-                <Stack justify="space-between" h={'100%'} gap={'8px'} >
+                <Stack justify="space-between" h={'100%'} gap={'8px'}>
                     <Box h={'42px'}>
                         <Text size={'16px'} style={{ lineHeight: 1.3 }}>{question.question}</Text>
                     </Box>
                     {activeCard == index ? (
-                        <>
+                        <Stack gap={'lg'} align="start">
                             <Box>
-                                <Text ta={'center'}>{index}</Text>
-                                <Text>{tip}</Text>
+                                <Text size="18px" ta={'center'} style={{ lineHeight: 1.5 }}>{question.interpretation[0].content}</Text>
                             </Box>
-                            <Button fullWidth onClick={() => setActiveCard(null)}>Got it!</Button>
-                        </>
+                            <Button variant={'filled'} fullWidth color="#515977" onClick={() => setActiveCard(null)}>Got it!</Button>
+                        </Stack>
                     ) : (
-                        <Stack gap={'12px'}>
+                        <Stack gap={'16px'}>
                             <Text size={'28px'} fw={500}>{`${(answer.SA + answer.A) * 100}% Agree`}</Text>
                             <Stack gap={0}>
                                 <Progress.Root size={24} style={{ borderRadius: '12px' }}>
@@ -92,23 +102,30 @@ const PaginatedData = ({ dataArray }: { dataArray: QuestionAnswerTally[] }) => {
                                     {(answer.D + answer.SD) > 0 && <Text fw={500} c="#FF5A5A">Disagree</Text>}
                                 </Group>
                             </Stack>
-                            <Stack gap={2} c={'grey'} align="flex-start">
-                                <Group justify="space-between" w={'100%'}>
-                                    <Text fw={500}>Strongly Agree</Text>
-                                    <Text fw={500}>{answer.SA * 100}%</Text>
-                                </Group>
-                                <Group justify="space-between" w={'100%'}>
-                                    <Text fw={500}>Agree</Text>
-                                    <Text fw={500}>{answer.A * 100}%</Text>
-                                </Group>
-                                <Group justify="space-between" w={'100%'}>
-                                    <Text fw={500}>Disagree</Text>
-                                    <Text fw={500}>{answer.D * 100}%</Text>
-                                </Group>
-                                <Group justify="space-between" w={'100%'}>
-                                    <Text fw={500}>Strongly Disagree</Text>
-                                    <Text fw={500}>{answer.SD * 100}%</Text>
-                                </Group>
+                            <Stack gap={2} c={'black'} align="flex-start">
+                                <Flex w={'100%'} gap={'lg'}>
+                                    <Group flex={.5} justify="space-between" w={'100%'}>
+                                        <Text size="sm" fw={500}>Strongly Agree</Text>
+                                        <Text size="sm" fw={500}>{answer.SA * 100}%</Text>
+                                    </Group>
+
+                                    <Group justify="space-between" w={'100%'} flex={.5}>
+                                        <Text size="sm" fw={500}>Agree</Text>
+                                        <Text size="sm" fw={500}>{answer.A * 100}%</Text>
+                                    </Group>
+                                </Flex>
+                                <Flex w={'100%'} gap={'lg'}>
+                                    <Group justify="space-between" w={'100%'}>
+                                        <Text size="sm" fw={500}>Disagree</Text>
+                                        <Text size="sm" fw={500}>{answer.D * 100}%</Text>
+                                    </Group>
+                                    <Group justify="space-between" w={'100%'}>
+                                        <Text size="sm" fw={500}>Strongly Disagree</Text>
+                                        <Text size="sm" fw={500}>{answer.SD * 100}%</Text>
+                                    </Group>
+                                </Flex>
+
+
                                 <Button mt={'md'} rightSection={<IconChevronDown />} variant={'filled'} color="#515977" onClick={() => setActiveCard(index)}>Show Insights</Button>
                             </Stack>
                         </Stack>
@@ -119,7 +136,7 @@ const PaginatedData = ({ dataArray }: { dataArray: QuestionAnswerTally[] }) => {
     </SimpleGrid>
 
     return (
-        <Stack w={'100%'} align="center" h={'500px'} justify="space-around" >
+        <Stack w={'100%'} align="center" h={'500px'} justify="space-around" gap={0}>
             <Group w={'100%'} h={'100%'}>
                 {items}
             </Group>
@@ -168,11 +185,13 @@ const SingleInsightCard = () => {
 
     return (
         <Paper p={'xl'} w={'100%'} h={'100%'}>
-            <Group justify="space-between" align="center" mb={'md'}>
-                <Text size="lg" fw={700}>Employee's Insight Per Question</Text>
-                <Button variant="light" color="black" radius='xl' onClick={() => navigate('/question-insight')}>View More</Button>
-            </Group>
-            <PaginatedData dataArray={data.results} />
+            <Stack w={'100%'} h={'100%'} gap={0}>
+                <Group justify="space-between" align="center">
+                    <Text size="lg" fw={700}>Employee's Insight Per Question</Text>
+                    <Button variant="light" color="black" radius='xl' onClick={() => navigate('/question-insight')}>View More</Button>
+                </Group>
+                <PaginatedData dataArray={data.results} />
+            </Stack>
         </Paper>
     )
 }
