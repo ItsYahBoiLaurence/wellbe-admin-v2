9c163caa31395913c3bec4a184c44004069a9fa0
ItsYahBoiLaurence <laurence@mayan.com.ph>
2025-12-16 10:42:06 +0800

    Wellbe Label Feature

--- src/components/V2Components/WellbeGraph/index.tsx
+++ src/components/V2Components/WellbeGraph/index.tsx
@@ -1,59 +1,133 @@
-import { useQuery } from "react-query"
-import api from "../../../api/api"
-import { Center, Paper, Stack, Text, Title } from "@mantine/core"
-import { LineChart } from "@mantine/charts"
-import LoaderComponent from '../LoaderComponent'
+import { useQueries, useQuery } from 'react-query';
+import api from '../../../api/api';
+import { Center, Paper, Stack, Text, Title } from '@mantine/core';
+import { LineChart } from '@mantine/charts';
+import LoaderComponent from '../LoaderComponent';
 
 export default function index({ period }: { period: string }) {
+  // const {} = useQuery({
+  //   queryKey: ['WELLBE_DATA', period],
+  //   queryFn: async () => {
+  //     const config = period ? { params: { period } } : {};
+  //     const res = await api.get('wellbeing/wellbe', config);
+  //     return res.data;
+  //   },
+  // });
 
-    const { data: WELLBE_DATA, isError: noWELLBE_DATA, isLoading: isFETCHINGDATA } = useQuery({
-        queryKey: ['WELLBE_DATA', period],
-        queryFn: async () => {
-            const config = period
-                ? { params: { period } }
-                : {}
-            const res = await api.get('wellbeing/wellbe', config)
-            return res.data
-        }
-    })
+  const [wellbeingData, wellbeingDataLabel] = useQueries([
+    {
+      queryKey: ['WELLBE_DATA', period],
+      queryFn: async () => {
+        const config = period ? { params: { period } } : {};
+        const res = await api.get('wellbeing/wellbe', config);
+        return res.data;
+      },
+    },
+    {
+      queryKey: ['WELLBE_DATA_LABEL', period],
+      queryFn: async () => {
+        const config = period ? { params: { period } } : {};
+        const res = await api.get(
+          'wellbeing/company-overall-wellbeing',
+          config
+        );
+        return res.data;
+      },
+    },
+  ]);
 
-    if (isFETCHINGDATA) return <LoaderComponent size="100%" />
-    if (noWELLBE_DATA) return <>No Data</>
+  const {
+    data: WELLBE_DATA,
+    isError: noWELLBE_DATA,
+    isLoading: isFETCHINGDATA,
+  } = wellbeingData;
 
+  const {
+    data: WELLBE_DATA_LABEL,
+    isError: noWELLBE_DATA_LABEL,
+    isLoading: isFETCHINGDATA_LABEL,
+  } = wellbeingDataLabel;
 
-    const data: { date: string, wellbeing: number }[] = []
+  if (isFETCHINGDATA || isFETCHINGDATA_LABEL)
+    return <LoaderComponent size="100%" />;
+  if (noWELLBE_DATA || noWELLBE_DATA_LABEL) return <>No Data</>;
 
-    WELLBE_DATA.map(({ created_at, wellbeing }: { created_at: string, wellbeing: number }) => {
-        const date = new Date(created_at)
-        const formatted = date.toLocaleDateString("en-US", {
-            month: "long",
-            day: "numeric",
-        });
-        data.push({ date: formatted, wellbeing })
-    })
+  const data: { date: string; wellbeing: number }[] = [];
 
-    console.log(data)
-    if (data.length === 0) return <Paper h={400}><Center h={"100%"}><Text>NO DATA AVAILABLE</Text></Center></Paper>
+  WELLBE_DATA.map(
+    ({ created_at, wellbeing }: { created_at: string; wellbeing: number }) => {
+      const date = new Date(created_at);
+      const formatted = date.toLocaleDateString('en-US', {
+        month: 'short',
+        day: 'numeric',
+      });
+      data.push({ date: formatted, wellbeing: wellbeing ?? 0 });
+    }
+  );
+
+  console.log('**********asdasdasdqwe1234as**********');
+  console.log(WELLBE_DATA_LABEL);
+  console.log('********************');
+
+  if (data.length === 0)
     return (
-        <Paper p='md'>
-            <Stack>
-                <Title order={2}>Company Wellbeing</Title>
-                <LineChart
-                    h={300}
-                    data={data}
-                    dataKey="date"
-                    series={[{ name: 'wellbeing' }]}
-                    curveType="monotone"
-                    xAxisProps={{
-                        domain: [0, 100],
-                        ticks: [0, 20, 40, 60, 80, 100],
-                    }}
-                    yAxisProps={{
-                        domain: [0, 100],
-                        ticks: [0, 20, 40, 60, 80, 100],
-                    }}
-                />
-            </Stack>
-        </Paper>
-    )
+      <Paper h={400}>
+        <Center h={'100%'}>
+          <Text>NO DATA AVAILABLE</Text>
+        </Center>
+      </Paper>
+    );
+
+  console.log('!!!!!!!!!!!!!!!');
+  console.log(data);
+  console.log('!!!!!!!!!!!!!!!');
+  return (
+    <Paper p="xl">
+      <Stack gap={'lg'}>
+        <div>
+          <Title order={3} fw={700}>
+            Company Wide Well-being Index
+          </Title>
+          <Title
+            order={1}
+            fw={700}
+            c={
+              WELLBE_DATA_LABEL.overall_wellbeing_label == 'Very High' ||
+              WELLBE_DATA_LABEL.overall_wellbeing_label == 'High' ||
+              WELLBE_DATA_LABEL.overall_wellbeing_label == 'Above Average'
+                ? '#82BC66'
+                : WELLBE_DATA_LABEL.overall_wellbeing_label == 'Average'
+                  ? '#94B8FB'
+                  : WELLBE_DATA_LABEL.overall_wellbeing_label ==
+                        'Below Average' ||
+                      WELLBE_DATA_LABEL.overall_wellbeing_label == 'Very Low'
+                    ? 'red'
+                    : 'red'
+            }
+          >
+            {WELLBE_DATA_LABEL.overall_wellbeing_label}
+          </Title>
+        </div>
+
+        <LineChart
+          h={300}
+          data={data}
+          dataKey="date"
+          series={[{ name: 'wellbeing' }]}
+          curveType="monotone"
+          //   xAxisProps={{
+          //     domain: [0, 100],
+          //     ticks: [0, 20, 40, 60, 80, 100],
+          //   }}
+          yAxisProps={{
+            domain: [0, 100],
+            ticks: [0, 20, 40, 60, 80, 100],
+          }}
+          withXAxis={true}
+          withYAxis={true}
+          gridAxis="xy"
+        />
+      </Stack>
+    </Paper>
+  );
 }
